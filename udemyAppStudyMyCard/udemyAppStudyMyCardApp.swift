4acf6537ebeb5ee9dc7b84b3e995a660fa598af8
ShaberAl <dsa_usa_clan_second_life@mail.ru>
2023-10-26 19:28:40 +0500

    Initial Commit

---
+++ udemyAppStudyMyCard/udemyAppStudyMyCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  udemyAppStudyMyCardApp.swift
+//  udemyAppStudyMyCard
+//
+//  Created by MacOS on 26.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct udemyAppStudyMyCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
